5f9abfd4cc2046f654b680ba8ee5ef747b2674b1
callen44 <caleballen44@outlook.com>
2023-05-06 13:23:34 -0500

    working on migrations

--- models/models.py
+++ models/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class PhoneNumbers(models.Model):
+    number = models.IntegerField(default=0)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
